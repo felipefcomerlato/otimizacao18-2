ce56e8203416032c7eef85a6f46dbbabdbbffc3b
felipefcomerlato <felipef1909@gmail.com>
2018-11-11 23:02:14 -0200

    Auto get important infos from instance

--- file_reader.py
+++ file_reader.py
@@ -1,7 +1,18 @@
+
+
+
+########################################################
+# Instructions:
+# 1 - Insert instance file in input_files directory
+# 2 - Run file_reader
+# 3 - Enter file name containing the extension
+########################################################
+
+
 import os
 
 #-------------------------------------------------------
-# READ
+
 def input_path():
     file_name = raw_input("Enter name of instance file: ")
     load("input_files/"+file_name)
@@ -20,13 +31,33 @@ def load(file_name):
         input_path()
 
 def getInfos(f):
-    content = [x.strip() for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()] # Read lines ignoring '\n'
+    first_line = content[:1]
+    first_line = first_line[0].split()
+
+    # Get garages, trips and garages capacities from file first line
+    garages = int(first_line[0])
+    capacities = []
+    for g in range(garages):
+        capacities.append(first_line[2+g])
+    trips = int(first_line[1])
+
+    # Print infos
+    print("Garages: "+str(garages))
+    print("Trips: "+str(trips))
+    for g in range(len(capacities)):
+        print("Garagem "+str(g+1)+" : capacidade "+str(capacities[g]))
+
+    # Table lines
+    table_lines = []
+
     for line in content:
+        table_lines.append(line.split())
+
+    for line in table_lines:
         print(line)
 
 
-
-# END READ
-#-----------------------------------------------------
+# -----------------
 
 input_path()
