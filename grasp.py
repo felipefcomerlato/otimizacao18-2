fedaa63135af6bf590c152f28b3cdd4d8595cedd
felipefcomerlato <felipef1909@gmail.com>
2018-11-27 14:47:51 -0200

    Introduces break logic and improves output

--- grasp.py
+++ grasp.py
@@ -31,8 +31,8 @@ def construct(current_node, local_cost):
     global count_infact
     global size_global_visiteds
     global capacities_aux
-    # print("garage:" + str(garage))
-    ## Atualiza solucao local com o nodo atual
+
+    # Add o nodo atual na lista de visitados da solucao local
     if current_node != garage:
         visiteds.append(current_node)
 
@@ -82,6 +82,10 @@ def construct(current_node, local_cost):
     for c in candidates:
         if graph[current_node][c] <= limit:
             rcl.append(c)
+            # Duplica chance de ser escolhido um caminho diferente da garagem
+            if c != garage:
+                rcl.append(c)
+
     ## Escolhe um caminho a seguir aleatoriamente dentre o contidos em RCL
     choice = random.randrange(0,len(rcl))
     local_cost = local_cost + graph[current_node][rcl[choice]]
@@ -144,6 +148,7 @@ def run(graph_instance, garages_instance, capacities_instance, trips_instance):
     rcl = []
     visiteds = []
     costs_table = []
+    total_vehicles = sum(capacities)
 
     print("\n######################################\n")
     # print(len(global_visiteds))
@@ -151,24 +156,24 @@ def run(graph_instance, garages_instance, capacities_instance, trips_instance):
     initial_time = time.time()
     open('output_grasp.txt', 'w').close()
     while True:
-        if time.time() - initial_time > 360:
+        if time.time() - initial_time > 3600:
             break
         capacities_aux = copy.deepcopy(capacities)
         del solution[:]
-        while size_global_visiteds < trips: ## AQUI VAI UM WHILE (enquanto nao forem visitados todos os locais)
-        # for k in range(2):
+        while size_global_visiteds < trips:
             if count_infact < trips*k_garages:
                 garage = random.randrange(0,k_garages) # 0, ... k_garages-1
                 local_cost = 0
-                if capacities_aux[garage] > 0: ## SE AINDA TEM BUS DISPONIVEL
+                if capacities_aux[garage] > 0:
                     construct(garage,local_cost)
                 del visiteds[:]
                 del pre_global_visiteds[:]
             else:
                 size_global_visiteds = trips
 
-            if sum(capacities_aux) == 0:
+            if sum(capacities_aux) == 0 or (current_optimal_cost > 0 and sum(costs_table) >= current_optimal_cost):
                 break
+            # END INTERN WHILE
 
         global_visiteds = list(itertools.chain(*global_visiteds))
 
@@ -176,7 +181,7 @@ def run(graph_instance, garages_instance, capacities_instance, trips_instance):
         if current_optimal_cost != -1:
             if total_cost < current_optimal_cost:
                 current_optimal_cost = total_cost
-                file_writer(total_cost, solution)
+                file_writer(total_cost, solution, total_vehicles - sum(capacities_aux))
                 print("\nTotal cost: "+str(total_cost))
                 # print("Final solution: "+str(solution))
         else:
@@ -190,10 +195,21 @@ def run(graph_instance, garages_instance, capacities_instance, trips_instance):
         size_global_visiteds = 0
         count_infact = 0
 
-def file_writer(total_cost, solution):
+def file_writer(total_cost, solution, vehicles):
     file = open("output_grasp.txt","a")
 
-    file.write("\nSolucao: "+str(solution))
-    file.write("\nCusto: "+str(total_cost))
-
+    file.write("\nCusto: "+str(total_cost)+" | Veiculos: "+str(vehicles))
+    file.write("\nSolucao: ")
+    for sequence in solution:
+        garage = sequence[0]
+        file.write("\n(K="+str(garage)+") -> ")
+        first = True
+        for trip in sequence:
+            if trip == garage:
+                if not first:
+                    file.write("(K="+str(garage)+")")
+            else:
+                file.write("(i="+str(trip)+") -> ")
+            first = False
+    file.write("\n")
     file.close()
